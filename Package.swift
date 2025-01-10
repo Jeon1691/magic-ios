ff2aebcc925a2e05bac55c2dae5016db02070ec2
develicit <develicit@wemade.com>
2025-01-10 20:19:05 +0900

    feat: remove package

--- Package.swift
+++ Package.swift
@@ -17,7 +17,6 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/magiclabs/Web3.swift.git", from:"1.1.0"),
-        .package(url: "https://github.com/mxcl/PromiseKit.git", from:"6.16.2")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -27,7 +26,6 @@ let package = Package(
             dependencies: [
                 .product(name: "MagicSDK_Web3", package: "Web3.swift"),
                 .product(name: "Web3PromiseKit", package: "Web3.swift"),
-                .product(name: "PromiseKit", package: "PromiseKit"),
                 .product(name: "Web3ContractABI", package: "Web3.swift")
             ]),
         .testTarget(
